01e155e6519274690ce51e3ac0e3726de84a68b3
Andrew Hexen <SyberHexen@gmail.com>
2021-12-08 11:14:23 -0800

    racer: omni_10 --> twrp_11
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I03fff1ff3f7e09e11fd6913ef87f466f23453322

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_racer.mk
+    $(LOCAL_DIR)/twrp_racer.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_racer-user \
-    omni_racer-userdebug \
-    omni_racer-eng
+    twrp_racer-user \
+    twrp_racer-userdebug \
+    twrp_racer-eng
